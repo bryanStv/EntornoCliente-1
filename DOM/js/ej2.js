7416e879d90f0e249c74b19fd7655db6526ae48d
bryanStv <bryans.oyola@gmail.com>
2024-09-25 19:51:05 +0200

    Ejercicio 2 Parte 2

--- DOM/js/ej2.js
+++ DOM/js/ej2.js
@@ -4,68 +4,24 @@ titulo.innerHTML = "EJERCICIO 2";
 let formulario = document.getElementById("basicForm");
 let resultado = document.getElementById("resultadoEJ2");
 
-let dni = document.getElementById("dni").value;
 let currentURL = location.href;
 
-function existDNI(){
-    if(dni == '' || dni == null){
-        return false;
-    }else{
-        return true;
-    }
-};
+formulario.addEventListener('submit', function(event) {
 
-formulario.addEventListener("submit", (event) => {
-    if(existDNI() == false){
+    let dni = document.getElementById('dni').value.trim();
+    
+    if (dni === '') {
+        event.preventDefault();
         alert("No se ha introducido DNI");
         document.getElementById("dni").focus();
-        console.log("false");
-    }else{
-        console.log("true");
-        resultado.innerText = "DNI: "+ dni + " --> " + 
-        currentURL.split('?')[0] + "?dni=" + dni;
-    }
+    } else {
+        //document.getElementById('resultadoEJ2').textContent = '';
+        //location.href(currentURL+"?dni="+dni);
+        //window.open(currentURL+"?dni="+dni,'_blank', 'width=800,height=600');
 
-    event.preventDefault();
+        event.preventDefault();
+        resultado.textContent = currentURL+"?dni="+dni;
+
+    };
+
 });
-
-/*var titulo = document.getElementById("t1");
-titulo.innerHTML = "EJERCICIO 2";
-
-let formulario = document.getElementById("basicForm");
-let resultado = document.getElementById("resultadoEJ2");
-let urlWeb = window.location.href;
-
-//let querystring = new URLSearchParams(location.search);
-//let querystring = new URLSearchParams(location.search);
-//let dni = querystring.get('dni');
-
-function eventoEJ2() {
-    let datoDNI = document.getElementById("dni").value;
-    if(datoDNI == ''){
-        alert("No se ha introducido DNI");
-        document.getElementById("dni").focus();
-    }else{
-        resultado.innerText = "DNI: "+datoDNI;
-        return datoDNI;
-    }
-}
-
-formulario.addEventListener('submit', function(event){
-    event.preventDefault();
-    eventoEJ2();
-    event.submitter(eventoEJ2());
-});*/
-
-
-/*formulario.addEventListener('submit', function(event){
-    event.preventDefault();
-    let datoDNI = document.getElementById("dni").value;
-
-    if(datoDNI == ''){
-        alert("No se ha introducido DNI");
-        document.getElementById("dni").focus();
-    }else{
-        resultado.innerHTML = `${datoDNI}`;
-    }
-});*/
