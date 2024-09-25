3b2c69b3e32450ab8d84b9eea08eda2d48c9078f
bryanStv <bryans.oyola@gmail.com>
2024-09-25 11:13:42 +0200

    Segundo intento de EJ2

--- DOM/js/ej2.js
+++ DOM/js/ej2.js
@@ -3,6 +3,36 @@ titulo.innerHTML = "EJERCICIO 2";
 
 let formulario = document.getElementById("basicForm");
 let resultado = document.getElementById("resultadoEJ2");
+
+function tratarDatos(){
+    let dni = document.getElementById("dni").value;
+
+    if(dni == ''){
+        alert("No se ha introducido DNI");
+        document.getElementById("dni").focus();
+        return false;
+    }else{
+        window.location.href = "../formulario2.html";
+        let resultado = document.getElementById("datosFinales");
+        resultado.innerText = "DNI: "+ dni;
+    }
+};
+
+formulario.addEventListener("submit", (event) => {
+    if(tratarDatos == false){
+        event.preventDefault();
+    }
+
+    tratarDatos();
+
+    sendData();
+});
+
+/*var titulo = document.getElementById("t1");
+titulo.innerHTML = "EJERCICIO 2";
+
+let formulario = document.getElementById("basicForm");
+let resultado = document.getElementById("resultadoEJ2");
 let urlWeb = window.location.href;
 
 //let querystring = new URLSearchParams(location.search);
@@ -24,7 +54,7 @@ formulario.addEventListener('submit', function(event){
     event.preventDefault();
     eventoEJ2();
     event.submitter(eventoEJ2());
-});
+});*/
 
 
 /*formulario.addEventListener('submit', function(event){
