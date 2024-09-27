5dc6be8e724c6b71b055ffe286a5c0185d2190d1
bryanStv <bryans.oyola@gmail.com>
2024-09-27 11:11:18 +0200

    Ejercicio 4-Por finalizar 2

--- DOM2/js/ej4.js
+++ DOM2/js/ej4.js
@@ -1,18 +1,22 @@
 function contar(tipo){
     let imagenes = document.getElementsByTagName('img');
     let enlaces = document.getElementsByTagName('a');
+    let resultado = document.getElementById('resultado');
+    let texto;
     if(tipo == 'img'){
-        return 'Número de imagenes: '+imagenes.length();
+        texto = document.createTextNode('Número de imagenes: '+imagenes.length());
+        resultado.appendChild(texto);
     }else if(tipo == 'a'){
-        return 'Número de enlaces: '+enlaces.length();
+        texto = document.createTextNode('Número de enlaces: '+enlaces.length());
+        resultado.appendChild(texto);
     }else{
-        return "Error";
+        resultado.appendChild('Error');
     }
 }
 
-let elementos = document.getElementById('elementos');
+/*let elementos = document.getElementById('elementos');
 let parrafo = document.createElement('p');
 let texto = document.createTextNode(contar(tipo));
 parrafo.append(texto);
 
-elementos.appendChild(parrafo);
+elementos.appendChild(parrafo);*/
