bba8e4ce65b4572ee5d25d142642712b77be4288
bryanStv <bryans.oyola@gmail.com>
2024-09-29 21:46:40 +0200

    Ejercicio 5 Finalizado

---
+++ DOM2/js/ej5.js
@@ -0,0 +1,6 @@
+var elem = document.getElementById("elemento");
+
+elem.setAttribute('name','formulario');
+elem.setAttribute('size','100');
+elem.setAttribute('placeholder','Hola Mundo')
+elem.setAttribute('style','background-color: red;')
