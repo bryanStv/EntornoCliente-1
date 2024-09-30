b2e0df76371659cfc4646791c09b9844942bd135
bryanStv <bryans.oyola@gmail.com>
2024-09-30 17:28:36 +0200

    Ejercicio 3 Anexo Finalizado

--- DOM2-Anexo/js/ej3.js
+++ DOM2-Anexo/js/ej3.js
@@ -4,10 +4,46 @@ function numeroEnlaces(){
 }
 
 function referenciaEnlaces(){
-    let referencia = document.getElementsByName('href').forEach(alert(referencia));
-    //alert(referencia)
+    let totalReferencias = 0;
+    let referencias = document.getElementsByTagName('a');
+
+    for(let i = 0; i < referencias.length; i++){
+        if(referencias[i].hasAttribute('href')){
+            totalReferencias++;
+        }
+    }
+    
+    alert('Número total de referencias: '+totalReferencias);
 }
 
 function numEnlacesParrafo(){
+    let contenedor = document.getElementById('contenedor');
+    let resultado = '';
 
+    if(contenedor.hasChildNodes()){
+        let parrafos = contenedor.getElementsByTagName('p');
+        let contador = 0;
+
+        while(contador < parrafos.length){
+            alert('Parrafo nº'+(contador+1)+': '+referenciasEnlacesEnParrafo(parrafos[contador]));
+            contador++;
+        }
+    }
+}
+
+function referenciasEnlacesEnParrafo(parrafo){
+    if(parrafo.hasChildNodes){
+        let totalReferencias = 0;
+        let referencias = parrafo.getElementsByTagName('a');
+    
+        for(let i = 0; i < referencias.length; i++){
+            if(referencias[i].hasAttribute('href')){
+                totalReferencias++;
+            }
+        }
+
+        return totalReferencias;
+    }else{
+        return 0;
+    }
 }
