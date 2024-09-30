a52b70e0d72d07ee5147f7bafe6415b6cfe29180
bryanStv <bryans.oyola@gmail.com>
2024-09-30 12:41:03 +0200

    Ejercicios DOM2 Finales

--- DOM2/js/ej5.js
+++ DOM2/js/ej5.js
@@ -1,6 +1,12 @@
-var elem = document.getElementById("elemento");
+var con = document.getElementById("contenedor");
+
+var elem = document.createElement('input');
+
+elem.setAttribute('type','text');
 
 elem.setAttribute('name','formulario');
 elem.setAttribute('size','100');
-elem.setAttribute('placeholder','Hola Mundo')
-elem.setAttribute('style','background-color: red;')
+elem.setAttribute('placeholder','Hola Mundo');
+elem.setAttribute('style','background-color: red;');
+
+con.appendChild(elem);
