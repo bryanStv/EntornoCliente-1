7adf195083ee940a540e6618fb27e28da29e4e4e
bryanStv <bryans.oyola@gmail.com>
2024-09-30 12:59:45 +0200

    Iniciado Anexo DOM2

---
+++ DOM2-Anexo/js/ej3.js
@@ -0,0 +1,13 @@
+function numeroEnlaces(){
+    let enlaces = document.getElementsByTagName('a');
+    alert('Número de enlaces: '+enlaces.length);
+}
+
+function referenciaEnlaces(){
+    let referencia = document.getElementsByName('href').forEach(alert(referencia));
+    //alert(referencia)
+}
+
+function numEnlacesParrafo(){
+
+}
