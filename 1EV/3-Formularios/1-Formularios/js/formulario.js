09228357ff8e8332e41a02739d1a63ad7ac62d79
bryanStv <bryans.oyola@gmail.com>
2024-10-23 23:55:29 +0200

    Verificar que no se introducen más de 150 caracteres

--- 1EV/3-Formularios/1-Formularios/js/formulario.js
+++ 1EV/3-Formularios/1-Formularios/js/formulario.js
@@ -1,10 +1,12 @@
 window.onload = function(){
     const formulario = document.getElementById('formulario1');
 
+    //Parte 1
     const nombre = document.getElementById('nombre');
 
     nombre.focus();
 
+    //Parte 2
     const imagen = document.getElementById('foto');
 
     imagen.addEventListener("change",function(){
@@ -12,10 +14,37 @@ window.onload = function(){
         const expRegular = /\.(jpe?g|png)$/;
 
         if(!expRegular.test(archivo)){
-            alert('Solo se permiten archivos de tipo .jpg o .png');
+            alert('Solo se permiten imagenes del tipo .jpg, jpeg o .png');
             imagen.value = '';
         }else{
-            alert('Imagen correcta')
+            alert('Imagen correcta');
+        }
+    })
+
+    //Parte 3
+    const aficion = document.getElementById('aficion');
+
+    aficion.addEventListener('change',function(){
+        const longitud = aficion.length;
+        const indice =  aficion.selectedIndex;
+        const seleccion = aficion.value;
+
+        alert(` La longitud de la lista es ${longitud}
+                El índice seleccionado es el ${indice}
+                El valor del índice seleccionado ${seleccion}`);
+    })
+
+    //Parte 4
+    const observaciones = document.getElementById('observaciones');
+    const maxCaracteres = 150;
+
+    observaciones.addEventListener('keydown',function(e){
+        const longitud = observaciones.value.length;
+
+        if(e.key == 'Backspace'){
+            return;
+        }else if(longitud == maxCaracteres){
+            e.preventDefault();
         }
     })
 
