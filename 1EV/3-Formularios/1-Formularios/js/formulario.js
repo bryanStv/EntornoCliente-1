e32ce143bc5de7a4a0882711fedff8cee2f6b362
bryanStv <bryans.oyola@gmail.com>
2024-10-23 23:15:23 +0200

    Verificación imagenes en formulario

--- 1EV/3-Formularios/1-Formularios/js/formulario.js
+++ 1EV/3-Formularios/1-Formularios/js/formulario.js
@@ -1,12 +1,26 @@
 window.onload = function(){
+    const formulario = document.getElementById('formulario1');
+
     const nombre = document.getElementById('nombre');
 
     nombre.focus();
 
     const imagen = document.getElementById('foto');
 
-    if(/^[a-zA-Z0-9]+\.jpe?g|\.png$/.text(imagen.value)){
-        
-    }
+    imagen.addEventListener("change",function(){
+        const archivo = imagen.value;
+        const expRegular = /\.(jpe?g|png)$/;
+
+        if(!expRegular.test(archivo)){
+            alert('Solo se permiten archivos de tipo .jpg o .png');
+            imagen.value = '';
+        }else{
+            alert('Imagen correcta')
+        }
+    })
+
+    formulario.addEventListener('submit',function(e){
+       
+    })
 }
 
